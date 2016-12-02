872a568802debe46fe65e92da06fb87839ffb2bd
Gearoid Kearney <gearoid.kearney@me.com>
2016-12-02 22:44:06 +0000

    ..

--- Dashboard.xaml.cs
+++ Dashboard.xaml.cs
@@ -64,6 +64,7 @@ namespace CollegeYak
                                              where a.POST_ID == votedPostId
                                              orderby a.POST_ID
                                              select a;
+
                     foreach (var voteUpItem in queryVoteUpCount)
                     {
                         upvote++;
@@ -71,15 +72,54 @@ namespace CollegeYak
 
                     var q = item.Username + "\n" + item.POST_TIME + "\n" + item.DETAILS;
 
-                    
+                     Button btnUpVote = new Button();
+                    btnUpVote.Content = "Up Votes";
+                    //btnUpVote.Click += new RoutedEventHandler(btnUpVote_Click);
+                    btnUpVote.Click += (sender, EventArgs) => { btnUpVote_Click(sender, EventArgs, item.POST_ID); };
+
+
+                    Label lblUpvote = new Label();
+                    lblUpvote.Content = upvote;
+
+                    Button btnDownVote = new Button();
+                    btnDownVote.Content = "Down Votes";
+                    //btnDownVote.Click += new RoutedEventHandler(btnDownVote_Click);
+
+                    btnDownVote.Click += (sender, EventArgs) => { btnDownVote_Click(sender, EventArgs, item.POST_ID); };
+
+
+
+                    Label lblDownvote = new Label();
+                    lblDownvote.Content = downvote;
+
 
                     listBox.Items.Add(q);
+                    listBox.Items.Add(btnUpVote);
+                    listBox.Items.Add(lblUpvote);
+                    listBox.Items.Add(btnDownVote);
+                    listBox.Items.Add(lblDownvote);
 
                 }
             }
 
         }
 
- 
+        void btnUpVote_Click(object sender, RoutedEventArgs e, Decimal id)
+        {
+
+            Button buttonThatWasClicked = (Button)sender;
+            MessageBox.Show("Up Vote Button pressed " + id);
+
+        }
+
+        void btnDownVote_Click(object sender, RoutedEventArgs e, Decimal id)
+        {
+
+            Button buttonThatWasClicked = (Button)sender;
+            MessageBox.Show("Down Vote Button pressed. The post id is  " +  id);
+
+        }
+
+
     }
 }
